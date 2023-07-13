53ec26e811b6f77cbe8f53043394e868b9b736e8
geniamaria <mariageniaabilio@gmail.com>
2023-07-13 11:20:42 +0200

    commit

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,2 +1,26 @@
 $("header").load("../components/header.html");
 $("footer").load("../components/footer.html");
+
+
+$("#menu").show();
+$("#ocultar-menu").hide();
+$("nav").hide();
+
+$("#menu").click(function(){
+    $('header').hide();
+$("menu").toggle();
+$("#ocultar-menu").toggle();
+
+ $("nav").animate({
+       marginTop:'0'
+    }, 1000, function() {
+        $("nav").show(1000);
+    });
+});
+$("#ocultar-menu").click(function(){
+    $('header').show();
+$("#menu").toggle();
+$("#ocultar-menu").toggle();
+
+$("nav").hide();
+});
